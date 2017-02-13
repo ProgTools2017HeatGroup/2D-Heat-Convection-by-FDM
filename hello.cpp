0d553bebe9060bdba46f26674b1dbb3721e448c3
arushi <al.arushievil@gmail.com>
2017-02-13 16:09:16 -0600

    First program built

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Arushi will type hello compiled, Jia will pull and push with 'Hello world'
+// and Hao will pull and push with 'Hello world again'
+using namespace std;
+int main()
+{
+ cout<<"\n"
+       "Hello";
+ cout<<endl;
+ return 0;
+}
