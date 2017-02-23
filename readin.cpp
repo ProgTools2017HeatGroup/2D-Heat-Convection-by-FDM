23dbeca6f0821bb2cc428d162cbad6520f206c8f
jia zhang <yortarz@gmail.com>
2017-02-23 01:10:12 -0600

    use subfunctions to print to logfile

--- readin.cpp
+++ readin.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 //  output values in scientific form
 using std::scientific;
+	
+int print_value(string str[100],int i,float &value,ofstream &file);
+int print_matrix(string str[100],int i,float (&matrix)[3][5],ofstream &file);
 		
 //  purpose: read in the parameters from input file and write these values to a log file
 //  usage: ./readin parameters.in logfile 
@@ -16,16 +19,15 @@ int main(int argc, char* argv[])
     string s;
     int line_length = 100;
     string str[line_length];
-    float rou, v, k, a;
+    float rho, v, k, a;
     float P[3][5], T[3][5], Vx[3][5], Vy[3][5];
-    int i,j,l;
-    char* c;
-    
+    int i;
+        
     
     if(argc < 3)
     { 
-           cout << "Missing inputfile or logfile, please check\n";
-           return EXIT_FAILURE;
+        cout << "Missing inputfile or logfile, please check\n";
+        return EXIT_FAILURE;
      }
     
     
@@ -36,8 +38,8 @@ int main(int argc, char* argv[])
     i = 0;
     while( infile >> s )
     { 
-         str[i] = s;
-         i++;
+        str[i] = s;
+        i++;
      }
     
     infile.close();
@@ -50,106 +52,61 @@ int main(int argc, char* argv[])
     i = 0;
     while(str[i] != "")
     {
-         if(str[i] == "rou=")
-         {
-             c = const_cast<char*>(str[i+1].c_str());
-             sscanf(c,"%e", &rou);
-             logfile << scientific << rou << endl;
-             logfile << "\n";
-             i++;
-         }
-         else if(str[i] == "v=")
-         {
-             c = const_cast<char*>(str[i+1].c_str());
-             sscanf(c,"%e", &v);
-             logfile << scientific << v << endl;
-             logfile << "\n";
-             i++;
-         }    
-         else if(str[i] == "k=")
-         {
-             c = const_cast<char*>(str[i+1].c_str());        	
-             sscanf(c,"%e", &k);
-             logfile << scientific << k << endl;
-             logfile << "\n";
-             i++;
-         }
-         else if(str[i] == "a=")
-         {
-             c = const_cast<char*>(str[i+1].c_str());        	
-             sscanf(c,"%e", &a);
-             logfile << scientific << a << endl;
-             logfile << "\n";
-             i++;
-         }    
-         else if(str[i] == "P=")
-         {
-             for(j = 0; j < 3; j = j+1)
-             {
-                for(l =0; l < 5; l = l+1)
-                {
-                    c = const_cast<char*>(str[i+1].c_str());        	                    
-                    sscanf(c,"%e", &P[j][l]);
-                    logfile << scientific << P[j][l] << "\t";
-                    i++;
-                 }
-                logfile << "\n"; 
-              }
-             logfile << "\n";
-         }
-         else if(str[i] == "T=")
-         {
-             for(j = 0; j < 3; j = j+1)
-             {
-                for(l =0; l < 5; l = l+1)
-                {
-                    c = const_cast<char*>(str[i+1].c_str());        	                    
-                    sscanf(c,"%e", &T[j][l]);
-                    logfile << scientific << T[j][l] << "\t";
-                    i++;
-                 }
-                logfile << "\n";
-              }
-             logfile << "\n";
-         }    
-         else if(str[i] == "Vx=")
-         {
-             for(j = 0; j < 3; j = j+1)
-             {
-                for(l =0; l < 5; l = l+1)
-                {
-                    c = const_cast<char*>(str[i+1].c_str());        	                    
-                    sscanf(c,"%e", &Vx[j][l]);
-                    logfile << scientific << Vx[j][l] << "\t";
-                    i++;
-                 }
-                logfile << "\n";
-              }
-             logfile << "\n";
-         }
-         else if(str[i] == "Vy=")
-         {
-             for(j = 0; j < 3; j = j+1)
-             {
-                for(l =0; l < 5; l = l+1)
-                {
-                    c = const_cast<char*>(str[i+1].c_str());        	                    
-                    sscanf(c,"%e", &Vy[j][l]);
-                    logfile << scientific << Vy[j][l] << "\t";
-                    i++;
-                 }
-                logfile << "\n";
-              } 
-             logfile << "\n";           
-         }
-         i++;   
+        if(str[i] == "rho=") print_value(str,i,rho,logfile);
+        if(str[i] == "v=")   print_value(str,i,v,logfile);
+        if(str[i] == "k=")   print_value(str,i,k,logfile);
+        if(str[i] == "a=")   print_value(str,i,a,logfile);
+         	
+        if(str[i] == "P=")   print_matrix(str,i,P,logfile);
+        if(str[i] == "T=")   print_matrix(str,i,T,logfile);
+        if(str[i] == "Vx=")  print_matrix(str,i,Vx,logfile);
+        if(str[i] == "Vy=")  print_matrix(str,i,Vy,logfile);
+         	
+        i++;
     }
+    
     logfile.close();
 
     return 0;
 }
 
 
+
+int print_value(string str[100],int i, float &value, ofstream &file) 	
+{
+    char* c;
+             
+    c = const_cast<char*>(str[i+1].c_str());
+    sscanf(c,"%e", &value);
+    file << scientific << value << endl;
+    file << "\n";
+    cout << scientific << value << endl;
+                             
+    return 0;
+}
+         
+
+int print_matrix(string str[100],int i, float (&matrix)[3][5], ofstream &file)
+{
+    int j,l;
+    char* c;
+         	   
+    for(j = 0; j < 3; j = j+1)
+    {
+        for(l =0; l < 5; l = l+1)
+        {
+            c = const_cast<char*>(str[i+1].c_str());        	                    
+            sscanf(c,"%e", &matrix[j][l]);
+            file << scientific << matrix[j][l] << "\t";
+            cout <<  scientific << matrix[j][l] << endl;
+            i++;
+         }
+        file << "\n"; 
+     }
+    file << "\n";
+             
+    return 0;
+}
 
 
 
