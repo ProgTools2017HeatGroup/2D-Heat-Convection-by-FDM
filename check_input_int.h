f060159b20484ea301b2b6ec14880d26d8d63010
luh1202 <hlu1@memphis.edu>
2017-04-21 11:00:06 -0500

    header file

---
+++ check_input_int.h
@@ -0,0 +1,9 @@
+nclude <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+using namespace std;
+
+int check_input(string str, int variable, int threshold);
+
