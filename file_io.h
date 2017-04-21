c9f4585e89cd27e619669da63d53cd68af3528d5
jzhang <jzhang10@memphis.edu>
2017-04-21 00:20:53 -0500

    simplify file_io.h

--- file_io.h
+++ file_io.h
@@ -7,20 +7,17 @@
 #include <sstream>
 #include <stdlib.h>
 #include <cmath>
+#include <vector>
 #include <cstdlib>
 
 using namespace std;
 
-int check_infile(int argc);
-int read_infile(int argc, char* argv[], string str[]);
-int store_params(string str[], float *rho, float *vis, float *diff, float *expa,
-    float *xe, float *ye, int *nx, int *ny, float ***P, float ***T, float ***Vx,
+int read_infile(char* args, vector<string> &str);
+int store_params(vector<string> &str, float *rho, float *vis, float *diff, float *expa,
+    int *xe, int *ye, int *nx, int *ny, float ***P, float ***T, float ***Vx,
     float ***Vy);
-//int write_input
-int read_float(string str[], int i, float &float_value);
-int read_int(string str[], int i, int &int_value);
-int read_matrix(string str[], int i, float ***matr, int *nx, int *ny);
-int allocate_matrix(float ***M, int nx, int ny);
-
+int write_logfile(char* agrs, float *rho, float *vis, float *diff,
+    float *expa, int *xe, int *ye, int *nx, int *ny, float ***P, float ***T, float ***Vx,
+    float ***Vy);
 
 #endif
