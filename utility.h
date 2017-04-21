dd07ffef23b1366fa0bbc1da4aa20bd8e8c92a41
jzhang <jzhang10@memphis.edu>
2017-04-21 00:22:05 -0500

    add utility.h

---
+++ utility.h
@@ -0,0 +1,25 @@
+#ifndef UTILITY_H_
+#define UTILITY_H_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+int read_float(vector<string> &str, int i, float &float_value);
+int read_int(vector<string> &str, int i, int &int_value);
+int read_matrix(vector<string> &str, int i, float ***matr, int *nx, int *ny);
+int check_vari_float(float &vari_float);
+int check_vari_int(int &vari_int);
+void print_error();
+int allocate_matrix(float ***M, int nx, int ny);
+void free_matrix(float ***M, int nx);
+
+
+#endif
