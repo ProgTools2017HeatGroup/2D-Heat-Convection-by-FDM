d59baab322d2f5a48b9aadd288e01b827569e491
arushi saxena <asaxena@memphis.edu>
2017-04-25 05:38:32 -0500

    collapsed in single function

--- src/generate_regular_grid.h
+++ src/generate_regular_grid.h
@@ -1,8 +1,6 @@
 #ifndef GENERATE_REGULAR_GRID_H
 #define GENERATE_REGULAR_GRID_H
 
-int** generate_x_points (int nx, int ny, double dx);
-
-int** generate_y_points (int nx, int ny, double dy);
+int** generate_points (int nx, int ny, double delta);
 
 #endif
